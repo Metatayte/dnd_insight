8f587cd5e53153c5173fb491bc2e4176881c5336
Metatayte <dtmalott@gmail.com>
2020-11-16 01:02:06 -0500

    :new: notes added / fav list done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import './random_words.dart';
 
+//Default main call for Flutter apps
 void main() => runApp(MyApp());
 
+//Stateless meaning it is immutable ;)
 class MyApp extends StatelessWidget {
+  // @override is implied but nice to explicitly declare it
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+//All of Flutter is basically just small widgets working together
+//This one is State Dependet (StatefulWidget)
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
 }
 
+//The State
 class RandomWordsState extends State<RandomWords> {
+  //The list of all the wordPairs created procedurally by english_words package
   final _randomWordPairs = <WordPair>[];
+  //List of 'saved' pairs by clicking on them (see onTap below)
   final _savedWordPairs = <WordPair>[];
 
+  //
   Widget _buildList() {
     return ListView.builder(
       padding: const EdgeInsets.all(8),
@@ -50,10 +56,45 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  //Functionality for top right list button to see favorited pairs
+  void _pushSaved() {
+    //Navigator is used to push on new pages and pop them off when user returns
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      //Use all pairs in _savedWordPairs and return a ListTile for each!!!
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
+
+      //Take the 'tiles' variable from above and run it through 'toList'
+      // This is just to take it from an interable into a List of widgets
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      //The finalized Scaffolding of the button being pressed.
+      return Scaffold(
+          appBar: AppBar(title: Text('Fav Campaigns')),
+          body: ListView(
+            //Take that list and return it in the body
+            children: divided,
+          ));
+    }));
+  }
+
+  //Actually the most senior Widget. Scaffold is a basic material based layout
+  //notice the body is set to _bodyList
+  // the _ means its privatized i think???
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Insight'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildList(),
     );
